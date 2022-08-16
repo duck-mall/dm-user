afe2337eff6e15fd34fc2742a320f8a08bc3c978
frank <1823427924@qq.com>
2022-08-16 16:05:57 +0800

    first

---
+++ src/main/java/com/duckmall/dm_user/DmUserApplication.java
@@ -0,0 +1,13 @@
+package com.duckmall.dm_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duckmall/dm_user/DmUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duckmall.dm_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
